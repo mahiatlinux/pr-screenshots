621b25de075aab6e8829a29d8f5dd021e36c5187
mahiatlinux <110882203+mahiatlinux@users.noreply.github.com>
2026-09-19 13:21:18 +1200

    publish proxy deletion verification

--- proxy_selection_probe.py
+++ proxy_selection_probe.py
@@ -36,3 +36,8 @@ for removal in ["clear()", "pop('https')"]:
     namespace = {}
     exec(code, namespace)
     print(json.dumps({'case':removal,'selected':select_proxy('https://pypi.org/', namespace['s'].proxies),'blocked':_check_code_safety(code + "\ns.get('https://pypi.org/')"),'prompt':is_high_risk_tool_call('python',{'code':code + "\ns.get('https://pypi.org/')"})}))
+
+code = "import requests\ns=requests.Session()\ns.trust_env=False\ns.proxies={'https':'http://203.0.113.5/'}\ndel s.proxies['https']"
+namespace = {}
+exec(code, namespace)
+print(json.dumps({'case':'del','selected':select_proxy('https://pypi.org/',namespace['s'].proxies),'blocked':_check_code_safety(code + "\ns.get('https://pypi.org/')"),'prompt':is_high_risk_tool_call('python',{'code':code + "\ns.get('https://pypi.org/')"})}))
